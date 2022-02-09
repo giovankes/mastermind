e8c43a7845fc622d823b3890eeac87017eb0d920
Giotje_ <devgio@icloud.com>
2022-02-09 19:47:09 +0100

    setup minimal flask rest api

---
+++ server/server.py
@@ -0,0 +1,13 @@
+from flas import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Server Works"
+
+
+@app.route("/greet")
+def say_hello():
+    return "Hello from server"
