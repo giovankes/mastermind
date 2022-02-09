0056e360650633252d6e4356021fbc0e212be7be
Giotje_ <devgio@icloud.com>
2022-02-09 15:36:18 +0100

    added mastermind

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  React.createElement(App, {
+    codeLength: 4,
+    colors: new Map([
+      [0, 'zero'],
+      [1, 'one'],
+      [2, 'two'],
+      [3, 'three'],
+      [4, 'four'],
+      [5, 'five'],
+    ]),
+  }),
+  document.getElementById('mastermind'),
 );
