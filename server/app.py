915cd92d58731821a7cdc1a4f8e7519302b9c1d9
Giotje <devgio@icloud.com>
2022-02-13 19:27:09 +0000

    server can now receive data from the front-end

--- server/app.py
+++ server/app.py
@@ -1,19 +1,14 @@
 from flask import Flask
 from flask import request
+from flask_cors import CORS
+import logging
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route("/")
+@app.route("/mastermind", methods=["POST"])
 def index():
-    return "Server Works"
-
-
-@app.route("/greet")
-def say_hello():
-    return "Hello from server"
-
-
-@app.route("/post-data", methods=["POST"])
-def post_data():
-    received_data = request.json
+    app.logger.info("hallo")
+    app.logger.info(request.json)
+    return "yes"
