5c50a2a900dd4798a75a151f46c2c0462df96085
Giotje <devgio@icloud.com>
2022-02-14 16:14:27 +0000

    python api now returns data

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flask import request
+from flask import request, jsonify
 from flask_cors import CORS
 import logging
+import random
 
 app = Flask(__name__)
 CORS(app)
@@ -12,7 +13,12 @@ def index():
     app.logger.info("hello!")
     app.logger.info(request.json["code"])
     code_to_guess = request.json["code"]
-
+    guess = list()
     for i in code_to_guess:
         app.logger.info(i)
-    return "yes"
+        random_number = random.randint(0, 4)
+        guess.append(random_number)
+
+    lst_to_tuple = tuple(i for i in guess)
+
+    return jsonify(guess=lst_to_tuple)
