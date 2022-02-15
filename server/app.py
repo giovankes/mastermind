6b7840d1c58e21fb511de1cacaf3ced6ce2f6a78
Giotje <devgio@icloud.com>
2022-02-15 14:02:19 +0000

    edited readme

--- server/app.py
+++ server/app.py
@@ -1,8 +1,18 @@
- from flask import Flask
+from flask import Flask
 from flask import request, jsonify
 from flask_cors import CORS
 import logging
 import random
+import itertools
+import collections
+
+
+Feedback = collections.namedtuple("Feedback", ["correct", "close"])
+
+
+def generate_initial_pool(choices, holes):
+    return list(itertools.product(*[range(choices) for _ in xrange(holes)]))
+
 
 app = Flask(__name__)
 CORS(app)
@@ -10,7 +20,6 @@ CORS(app)
 
 @app.route("/mastermind", methods=["POST"])
 def index():
-    app.logger.info("hello!")
     app.logger.info(request.json["code"])
     code_to_guess = request.json["code"]
     guess = list()
